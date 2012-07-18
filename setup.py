c310b61d040be835f18a531ccfdfdbdc9a47559c
Ezequiel Bertti <ezequiel.bertti@sieve.com.br>
2012-07-18 18:47:16 -0300

    create setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='micro-cache',
+    version='0.1',
+    packages=['microcache'],
+    url='https://github.com/ebertti/micro-cache',
+    license='MIT',
+    author='Ezequiel Bertti',
+    author_email='ebertti@gmail.com',
+    description='Simple Memory Cache '
+)
